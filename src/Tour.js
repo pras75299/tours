5c420083c097b51c629ba76453a6e21e4fa8691e
pras75299 <singh.prashantking@gmail.com>
2020-11-21 19:37:08 +0530

    fetch tours and show

--- src/Tour.js
+++ src/Tour.js
@@ -1,7 +1,21 @@
 import React, { useState } from "react";
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = (props) => {
+  const { id, name, info, image, price } = props;
+
+  return (
+    <article className="single-tour">
+      <img src={image} alt={name} />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">$ {price}</h4>
+        </div>
+        <p>{info}</p>
+        <button className="delete-btn">Delete</button>
+      </footer>
+    </article>
+  );
 };
 
 export default Tour;
--- src/Tours.js
+++ src/Tours.js
@@ -1,7 +1,21 @@
 import React from "react";
 import Tour from "./Tour";
-const Tours = () => {
-  return <h2>tours component</h2>;
+const Tours = (props) => {
+  const { tours } = props;
+  return (
+    <section>
+      <div className="title">
+        <h2>Our Tours</h2>
+        <div className="underline"></div>
+      </div>
+      <div>
+        {tours.map((tour) => {
+          const { id, name, info, image, price } = tour;
+          return <Tour key={id} {...tour} />;
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Tours;
