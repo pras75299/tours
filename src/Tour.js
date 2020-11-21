3577d0836bf159a51f990139a5eff03281a4d125
pras75299 <singh.prashantking@gmail.com>
2020-11-21 19:39:41 +0530

    add show less and read more for tour info

--- src/Tour.js
+++ src/Tour.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 const Tour = (props) => {
   const { id, name, info, image, price } = props;
-
+  const [readMore, setReadMore] = useState(false);
   return (
     <article className="single-tour">
       <img src={image} alt={name} />
@@ -11,7 +11,12 @@ const Tour = (props) => {
           <h4>{name}</h4>
           <h4 className="tour-price">$ {price}</h4>
         </div>
-        <p>{info}</p>
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button onClick={() => setReadMore(!readMore)}>
+            {readMore ? "show less" : "Read More"}
+          </button>
+        </p>
         <button className="delete-btn">Delete</button>
       </footer>
     </article>
